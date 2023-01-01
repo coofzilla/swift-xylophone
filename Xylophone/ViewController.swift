e991788eaf44b51016dbc78e0808f1cdc1374bf9
coofzilla <jeric614@yahoo.com>
2023-01-01 20:37:31 +0900

    play sound and link with IBAction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -5,16 +5,28 @@
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 
+    func playSound(fileName: String) {
+        let url = Bundle.main.url(forResource: fileName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
 
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(fileName: "C")
+    }
+    
     
 
 }
