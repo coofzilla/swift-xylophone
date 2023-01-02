e31edb806aca64f3963c2a372637315e0ade4f0d
coofzilla <jeric614@yahoo.com>
2023-01-02 14:28:27 +0900

    feature: link keys to IBAction and playSound based on note pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
+    
     func playSound(fileName: String) {
         let url = Bundle.main.url(forResource: fileName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player?.play()
-
+        
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound(fileName: "C")
+        if let title = sender.titleLabel?.text! {
+            playSound(fileName: title)
+        }
+
     }
     
     
-
+    
 }
 
